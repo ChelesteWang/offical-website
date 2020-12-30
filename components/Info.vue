af7db408fdddc83553c8d6e187f2930f22453760
王心圆 <wangxinyuan@zsrtech.com>
2020-12-31 00:19:19 +0800

    info moblie repair

--- components/Info.vue
+++ components/Info.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex justify-center">
-    <div class="">
+    <div class="pc-only hidden md:block">
       <h1 style="font-size: 48px">Beyond software</h1>
       <h2 style="font-size: 36px">
         <p >A team with more than</p><p style="font-size: 40px;color:#1162F9;float:left">10&nbsp</p>
@@ -18,9 +18,28 @@
       </div>
       <p style="font-size: 20px;transform: translateY(60px)">We own application which has 100,000 users</p>
     </div>
+    <div class="mobile-only md:hidden">
+      <h1 style="">Beyond software</h1>
+      <h2 style="">
+        <p >A team with more than</p>
+        <p style="color:#1162F9;float:left">10&nbsp</p>
+        <p>years of experience</p>
+        <br style="clear:both">
+      </h2>
+      <h3 style="">
+        Our Bible is : Comtinuous delivery , committed to customer success
+      </h3>
+      <div style="transform: translateY(60px)">
+        <p>We Design Software</p>
+        <p>We planning Software</p>
+        <p>We Develop Software</p>
+        <p>We Operating Software</p>
+      </div>
+      <p style="transform: translateY(60px)">We own application which has 100,000 users</p>
+    </div>
     <div
       style="transform: translateY(-150px) scale(0.65);"
-      class="hidden -mx-4 md:flex md:items-center"
+      class="hidden -mx-4 md:flex"
     >
       <iframe
         src="https://modao.cc/app/0d959208210c78c5545d0e41daaf4114be6a8542/embed"
