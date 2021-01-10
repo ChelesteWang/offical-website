c694ec4477360cfd6d034f3f0b029e274e3038f4
王心圆 <wangxinyuan@zsrtech.com>
2021-01-10 21:15:59 +0800

    update

--- components/Info.vue
+++ components/Info.vue
@@ -23,12 +23,14 @@
         We own application which has 100,000+ users
       </p>
     </div>
-    <div class="mobile-only md:hidden" style="color: #718096;width:80%">
-      <h1 style="text-align: center;font-weight:400;font-size:20px;">Beyond software</h1>
-      <h2 style="text-align: center;margin-top:20px">
+    <div class="mobile-only md:hidden" style="color: #718096; width: 80%">
+      <h1 style="text-align: center; font-weight: 400; font-size: 20px">
+        Beyond software
+      </h1>
+      <h2 style="text-align: center; margin-top: 20px">
         A team with more than 10 years of experience
       </h2>
-      <h3 style="text-align: center;margin-top:20px">
+      <h3 style="text-align: center; margin-top: 20px">
         Our Bible is : Comtinuous delivery , committed to customer success
       </h3>
       <!-- <div style="transform: translateY(60px);text-align: center;">
@@ -37,23 +39,41 @@
         <p>We Develop Software</p>
         <p>We Operating Software</p>
       </div> -->
-      <p style="text-align: center;margin-top:20px">
+      <p style="text-align: center; margin-top: 20px">
         We own application which has 100,000+ users
       </p>
     </div>
-    <div
-      style="text-align: center;"
-      class="hidden -mx-2 md:flex"
-    >
+    <div style="text-align: center;position: relative;left:100px" class="hidden -mx-2 md:flex">
       <iframe
         src="https://modao.cc/app/0d959208210c78c5545d0e41daaf4114be6a8542/embed"
         width="515"
         height="900"
         allowTransparency="true"
         frameborder="0"
-        style="transform: translateY(-150px) scale(0.60);"
+        style="transform: translateY(-150px) scale(0.6)"
       ></iframe>
-      <h1 class="info">Try to click</h1>
+      <h1 class="info">
+        <div>
+          <svg
+            t="1610283163118"
+            class="icon"
+            viewBox="0 0 1024 1024"
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+            p-id="1098"
+            width="50"
+            height="50"
+            style="position:relative;left:30px"
+          >
+            <path
+              d="M510.06 112h-0.578a37.81 37.81 0 0 0-21.442 7.042l-0.364 0.263 0.105-0.067a45.582 45.582 0 0 0-6.93 5.486l-0.598 0.587-270.946 270.62c-17.743 17.721-17.743 46.456 0 64.178l0.536 0.526c17.78 17.19 46.142 17.015 63.705-0.526l198.597-198.36v612.384c0 20.914 16.974 37.867 37.908 37.867h5.309c20.645-0.34 37.28-17.162 37.28-37.867V261.748l198.598 198.36c17.74 17.72 46.5 17.72 64.241 0 17.743-17.72 17.744-46.455 0.002-64.177L544.515 125.289l-0.575-0.564a45.577 45.577 0 0 0-6.257-5.045l-0.569-0.375 0.104 0.076A37.78 37.78 0 0 0 514.731 112h-4.671z"
+              p-id="1099"
+              fill="#2c2c2c"
+            ></path>
+          </svg>
+        </div>
+        Try to click
+      </h1>
     </div>
   </div>
 </template>
@@ -65,11 +85,11 @@ export default {
 </script>
 
 <style>
-  .info{
-    position: absolute;
-    top:1280px;
-    left: 880px;
-    font-size:24px;
-    font-weight:bold ;
-  }
+.info {
+  position: relative;
+  left:-328px;
+  top: 550px;
+  font-size: 24px;
+  font-weight: bold;
+}
 </style>
